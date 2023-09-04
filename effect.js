3fec0236daa744d520a15ad7272c84c92d108c46
Pufikas <dominykas.guzys@gmail.com>
2023-09-04 19:15:36 +0300

    edit

--- effect.js
+++ effect.js
@@ -4,6 +4,7 @@ class Effect {
         this.video = video;
         this.ctx = canvas.getContext("2d");
         this.particles = [];
+        this.prevLoc = { x: 0, y: 0 };
         this.#animate();
     }
 
@@ -23,6 +24,9 @@ class Effect {
 
         if (locs.length > 0) {
             const center = average(locs);
+            const vel = center.x - this.prevLoc.x;
+            this.prevLoc = center;
+
             for (let i = 1; i <= 42; i++) {
                 this.particles.push(new Particle(center));
             }
@@ -30,28 +34,44 @@ class Effect {
             ctx.beginPath();
             ctx.fillStyle = `hsla(200, 100%, 50%)`;
             ctx.arc(center.x, center.y, 20, 0, Math.PI * 2);
-           
             ctx.fill();
     // center
             // ctx.beginPath();
             // ctx.fillStyle = "red";
             // ctx.arc(center.x, center.y, 5, 0, Math.PI * 2);
             // ctx.fill();
+
             function getLength() {
                 const lengthDiv = document.getElementById("lengthDiv");
-                const point = [center.x, center.y - 20];
-                const distance = euclideanDistance([245, 5], point);
-                lengthDiv.innerHTML = Math.floor(distance) + ("px");
-                console.log(distance)
+                const distance = Math.sqrt(245 * 245 + 5 * 5);
+                let centimeters = distance / (96 / 2.54)
+                lengthDiv.innerHTML = "Length" + "\n" + Math.floor(centimeters) + "cm" + "/" + Math.floor(distance) + "px";
+                //console.log(distance)
                 ctx.strokeStyle = "gray";
-                //ctx.arc(245, 5, 1, 0, 1); // dot in the middle of screen
-        
                 ctx.lineWidth = 2;
                 ctx.moveTo(246, 5)
                 ctx.lineTo(center.x, (center.y - 20))
                 ctx.stroke();
             }
-            getLength()
+
+            function centerDot() {
+                const posX = document.getElementById("xPos");
+                const posY = document.getElementById("yPos");
+                const center = average(locs);
+                posX.innerHTML = `Position x: ${center.x}`;
+                posY.innerHTML = `Position y: ${center.y}`;
+            }
+
+            function velocity() {
+                const velocityDiv = document.getElementById("velocity");
+                
+
+                velocityDiv.innerHTML = `Velocity: ${vel}`
+            }
+
+            velocity();
+            centerDot();
+            getLength();
         }
 
         this.particles.forEach(p => {
